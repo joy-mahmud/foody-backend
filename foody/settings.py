08dcaab9068926eca016e68fbcab4b7f4e6d5208
joy-mahmud <joymahmudiucse@gmail.com>
2025-07-07 23:12:49 +0600

    load json data into the database

--- foody/settings.py
+++ foody/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firebaseUser',
-    'corsheaders'
+    'corsheaders',
+    'foodItems'
 ]
 
 MIDDLEWARE = [
