95aa1a773dcf283a2f1692b48719a55a96e2de2a
joy-mahmud <joymahmudiucse@gmail.com>
2025-09-25 23:55:15 +0600

    total revenue count

--- stats/views.py
+++ stats/views.py
@@ -1,24 +1,24 @@
 from foodItems.models import FoodItem
 from firebaseUser.models import FirebaseUser
-from cart.models import CartItem
+from cart.models import CartItem,Order
 from django.db.models import Avg, Sum
 from django.http import JsonResponse
 
 def stats_summary(request):
     if request.method == "GET":
         try:
+            total_revenue = Order.objects.aggregate(total=Sum("total_amount"))["total"] or 0
             food_count = FoodItem.objects.count()
             avg_rating = FoodItem.objects.aggregate(avg=Avg("rating"))["avg"] or 0
-            avg_price = FoodItem.objects.aggregate(avg=Avg("price"))["avg"] or 0
             user_count = FirebaseUser.objects.count()
-            total_cart_qty = CartItem.objects.aggregate(total=Sum("quantity"))["total"] or 0
+            total_orders = Order.objects.count()
 
             return JsonResponse({
+                "total_revenue":total_revenue,
                 "food_count": food_count,
                 "avg_rating": round(float(avg_rating),2),
-                "avg_price": round(float(avg_price),2),
                 "user_count": user_count,
-                "total_cart_qty": total_cart_qty
+                "total_orders": total_orders
             })
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
