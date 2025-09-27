45942a2cb01603adb2a927650cfa36c777a13d1a
joy-mahmud <joymahmudiucse@gmail.com>
2025-09-27 23:26:29 +0600

    dashboard recent users and orders

--- firebaseUser/models.py
+++ firebaseUser/models.py
@@ -7,6 +7,7 @@ class FirebaseUser(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20,choices=ROLE_CHOICES,default='user')
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return self.email
--- stats/views.py
+++ stats/views.py
@@ -12,13 +12,37 @@ def stats_summary(request):
             avg_rating = FoodItem.objects.aggregate(avg=Avg("rating"))["avg"] or 0
             user_count = FirebaseUser.objects.count()
             total_orders = Order.objects.count()
+            recent_orders = Order.objects.select_related("user").order_by('-created_at')[:3]
+            order_data =[
+                {
+                   "id":order.id,
+                   "user":order.user.name if order.user else None,
+                   "total_amount":float(order.total_amount),
+                   "status":order.status,
+                   "created_at":order.created_at
+                } 
+                for order in recent_orders
+            ]
+            
+            recent_users = FirebaseUser.objects.order_by('-created_at')[:3]
+            users_data =[
+                {
+                    "id":user.id,
+                    "name":user.name,
+                    "email":user.email,
+                    "created_at":user.created_at
+                } 
+                for user in recent_users
+            ]
 
             return JsonResponse({
                 "total_revenue":total_revenue,
                 "food_count": food_count,
                 "avg_rating": round(float(avg_rating),2),
                 "user_count": user_count,
-                "total_orders": total_orders
+                "total_orders": total_orders,
+                "recent_orders":order_data,
+                "recent_users":users_data
             })
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
